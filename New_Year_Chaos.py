8fef524136496ddd9cc2cbabd6bc544f40e8d388
Ash <ygshah@asu.edu>
2018-10-22 22:18:37 -0700

    Update New_Year_Chaos.py

---
+++ New_Year_Chaos.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    length = len(q)
+    arra=[]
+    temp=[]
+    bribes = 0
+    for k in range(1,length+1):
+        arra.append([k])
+    for j in range(0,length):
+        val = arra[j] - q[j]
+        if abs(val)>2:
+            print ("Too chaotic")
+        else:
+            if arra[j] not in temp :
+                temp.append(arra [j])
+            if q[j]  not in temp:
+                temp.append(q[j])
+                bribes += 1
+    return  bribes  
+     
+            
+    
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
