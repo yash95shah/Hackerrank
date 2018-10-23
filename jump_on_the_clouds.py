2ccef35665d79e44df7c0f3b14d072c215a7fdac
Ash <ygshah@asu.edu>
2018-10-22 17:46:13 -0700

    Update jump_on_the_clouds.py

---
+++ jump_on_the_clouds.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    count = 0
+    l= len(c)
+    c.append(1)
+    c.append(1)
+   
+    for k in range (0,l+2):
+        if c[k] == 0:
+            if c[k+2] == 0:
+                count += 1
+            elif c[k+1]== 0 and c[k-1] != 0:
+                count += 1
+        
+    return count 
+    
+            
+            
+            
+    return i
+        
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
